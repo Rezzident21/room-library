99bf6416f8216d9f38f036cb2a4fe783fb2d73ac
Rezzident <seredyuk.davi@gmail.com>
2020-09-26 15:01:24 +0300

    Update Functions.php

--- functions/Functions.php
+++ functions/Functions.php
@@ -5,7 +5,7 @@ class Functions
 
     public function drawPager($totalItems, $perPage)
     {
-        /*Calculator the nmber of pages*/
+        /*Calculator the number of pages*/
         $pages = ceil($totalItems / $perPage);
 
         if (!isset($_GET['page']) || intval($_GET['page']) == 0) {
@@ -18,11 +18,11 @@ class Functions
 
         $pager = "<nav aria-label='Page navigation'>";
         $pager .= "<ul class='pagination'>";
-        $pager .= "<li><a href='/books?page=1' aria-label='Previous'><span aria-hidden='true'>«</span> Начало</a></li>";
+        $pager .= "<li><a href='/books?page=1' aria-label='Previous'><span aria-hidden='true'>«</span> First</a></li>";
         for ($i = 2; $i <= $pages - 1; $i++) {
             $pager .= "<li><a href='/books?page=" . $i . "'>" . $i . "</a></li>";
         }
-        $pager .= "<li><a href='/books?page=" . $pages . "' aria-label='Next'>Конец <span aria-hidden='true'>»</span></a></li>";
+        $pager .= "<li><a href='/books?page=" . $pages . "' aria-label='Next'>End <span aria-hidden='true'>»</span></a></li>";
         $pager .= "</ul>";
 
         return $pager;
